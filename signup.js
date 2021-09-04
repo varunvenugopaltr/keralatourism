0a5c221d0abe03a1417e4d8189baed23d4075f9d
varun <varunvenugopaltr@gmail.com>
2021-09-04 21:16:57 +0530

    Kerala Tourism : V12

--- signup.js
+++ signup.js
@@ -2,7 +2,7 @@ var patt = /[a-z0-9._%+-]+@[a-z0-9.]+\.[a-z]{2,3}$/
 
 $('body').on('keyup change paste cut', '#myEmail', function() {
 
-	var res = patt.test($(this).val()) === true  ? 'Correct Email format.' : 'Invalid Email format. Please enter a valid Email address.';
+	var res = patt.test($(this).val()) === true  ? 'Correct Email format.' : 'Invalid Email format. Please enter a valid Email address(Format sample@domain.com).';
   $('.result').text(res)
 });
 
@@ -15,3 +15,5 @@ $('body').on('keyup change paste cut', '#myMob', function() {
 	var res1 = mobpatt.test($(this).val()) === true  ? 'Correct Mobile number format.' : 'Invalid Mobile Number format. Suggested format: XXX-XXX-XXXX or XXX.XXX.XXXX or XXX XXX XXX';
   $('.result1').text(res1)
 });
+
+
